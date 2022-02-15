0ee43b5d302d745c6dbf09326659992babfbc807
Tatsiana Trakhimets <ttatsianatt@gmail.com>
2022-02-15 15:36:01 +0100

    title

--- db/migrate/20220215142522_add_task_to_tasks.rb
+++ db/migrate/20220215142522_add_task_to_tasks.rb
@@ -1,13 +1,5 @@
-class AddTaskToTasks < ActiveRecord::Migration[6.1]
-  def change
-    add_column
-  end
-end
-
-class AddAddressToRestaurants < ActiveRecord::Migration[6.0]
+class AddTitleToTasks < ActiveRecord::Migration[6.1]
   def change
     add_column :tasks, :title, :string
-    add_column :tasks, :details, :text
-    add_column :tasks, :completed, :boolean
   end
 end
